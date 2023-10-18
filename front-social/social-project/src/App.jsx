635e6a782d594d0a8b8063f9410c1e510fea7a27
Ruslan kHasanov <khazaruscsu@gmail.com>
2023-10-18 18:24:57 +0500

    auth page

--- front-social/social-project/src/App.jsx
+++ front-social/social-project/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css';
+import 'normalize.css';
+import AuthPage from './pages/auth/AuthPage';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      
-    </>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={'main'} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
